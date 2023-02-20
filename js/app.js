1d4697e2ea11393421aa217af1864f42e2392b72
ABiviano13 <abiviano0@outlook.com>
2023-02-20 14:42:03 +0100

    START

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const { createApp } = Vue;
+console.log(Vue);
+
+
+const option = {
+    data() {
+	    return {
+		    
+	    }
+    },
+    methods: {
+	    
+    },
+};
+const app = createApp(option);
+app.mount('#app');
