8f77343bd39d687100747b16cd3c4b9c2aae4359
ABiviano13 <abiviano0@outlook.com>
2023-02-21 15:19:19 +0100

    COMPLETED

--- js/app.js
+++ js/app.js
@@ -5,32 +5,32 @@ let arrayImageslide = [
     {
         image: './img/image1.jpg',
         title: 'Torre Eiffel',
-        text: 'Punto di riferimento storico - Francia, Parigi'
+        text: 'Punto di riferimento storico - <strong> Francia, Parigi </strong>'
     },
     {
         image: './img/image4.jpg',
         title: 'Tempio Indù',
-        text: 'Uno dei simboli più rappresentativi di Bali - Indonesia'
+        text: 'Uno dei simboli più rappresentativi di Bali - <strong> Indonesia </strong>'
     },
     {
         image: './img/image5.jpg',
         title: 'Ponte di Rialto',
-        text: 'Uno dei 30 ponti più belli di Italia - Venezia'
+        text: 'Uno dei 30 ponti più belli di Italia - <strong> Venezia </strong>'
     },
     {
         image: './img/image6.jpg',
         title: 'Basilica di Santa Maria della Salute',
-        text: 'Punto di riferimento storico - Italia, Venezia'
+        text: 'Punto di riferimento storico - <strong> Italia, Venezia </strong>'
     },
     {
         image: './img/image7.jpg',
         title: 'Bora Bora',
-        text: 'Isola più bella della Polinesia Francese - Bora Bora'
+        text: 'Isola più bella della Polinesia Francese - <strong> Bora Bora </strong>'
     },
     {
         image: './img/image8.jpg',
         title: 'InterContinental Bora Bora Resort',
-        text: 'Hotel Thalasso di Bora Bora, Indonesia isola felice'
+        text: 'Hotel Thalasso di Bora Bora, <strong> Indonesia isola felice </strong> '
     },
     {
         image: './img/image10.jpg',
@@ -44,18 +44,27 @@ const option = {
     data() {
 	    return {
             array: arrayImageslide, //Più leggibile
-            // className: 'slide-carousel',
-            // classNameActive: 'active',
-            // imageSlide: array.image,
             currentIndex: 0,
 	    }
     },
     methods: {
         nextSlide() {
             console.log('next');
+            const lastIndex = this.array.length - 1
+            if(this.currentIndex < lastIndex) {
+                this.currentIndex++;
+            } else {
+                this.currentIndex = 0;
+            }
         },
         prevSlide() {
             console.log('prev');
+            const lastIndex = this.array.length - 1
+            if(this.currentIndex > 0) {
+                this.currentIndex--;
+            } else {
+                this.currentIndex = lastIndex;
+            }
         }
 	    
     },
