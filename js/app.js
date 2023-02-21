6b401344fae29bccb835cee15f39e291128bed44
ABiviano13 <abiviano0@outlook.com>
2023-02-21 11:23:15 +0100

    FEAT Image + Correct v-for

--- js/app.js
+++ js/app.js
@@ -1,56 +1,62 @@
 const { createApp } = Vue;
 console.log(Vue);
 
-let array = [];
+let arrayImageslide = [
+    {
+        image: './img/image1.jpg',
+        title: 'Torre Eiffel',
+        text: 'Punto di riferimento storico - Francia, Parigi'
+    },
+    {
+        image: './img/image4.jpg',
+        title: 'Tempio Indù',
+        text: 'Uno dei simboli più rappresentativi di Bali - Indonesia'
+    },
+    {
+        image: './img/image5.jpg',
+        title: 'Ponte di Rialto',
+        text: 'Uno dei 30 ponti più belli di Italia - Venezia'
+    },
+    {
+        image: './img/image6.jpg',
+        title: 'Basilica di Santa Maria della Salute',
+        text: 'Punto di riferimento storico - Italia, Venezia'
+    },
+    {
+        image: './img/image7.jpg',
+        title: 'Bora Bora',
+        text: 'Isola più bella della Polinesia Francese - Bora Bora'
+    },
+    {
+        image: './img/image8.jpg',
+        title: 'InterContinental Bora Bora Resort',
+        text: 'Hotel Thalasso di Bora Bora, Indonesia isola felice'
+    },
+    {
+        image: './img/image10.jpg',
+        title: 'Keizersgracht',
+        text: 'Arte = Amsterdam - Olanda'
+    }
+];
 
 
 const option = {
     data() {
 	    return {
-            array: [
-                {
-                    image: './img/image1.jpg',
-                    title: 'Torre Eiffel',
-                    text: 'Punto di riferimento storico - Francia, Parigi'
-                },
-                {
-                    image: './img/image4.jpg',
-                    title: 'Tempio Indù',
-                    text: 'Uno dei simboli più rappresentativi di Bali - Indonesia'
-                },
-                {
-                    image: './img/image5.jpg',
-                    title: 'Ponte di Rialto',
-                    text: 'Uno dei 30 ponti più belli di Italia - Venezia'
-                },
-                {
-                    image: './img/image6.jpg',
-                    title: 'Basilica di Santa Maria della Salute',
-                    text: 'Punto di riferimento storico - Italia, Venezia'
-                },
-                {
-                    image: './img/image7.jpg',
-                    title: 'Bora Bora',
-                    text: 'Isola più bella della Polinesia Francese - Bora Bora'
-                },
-                {
-                    image: './img/image8.jpg',
-                    title: 'InterContinental Bora Bora Resort',
-                    text: 'Hotel Thalasso di Bora Bora, Indonesia isola felice'
-                },
-                {
-                    image: './img/image10.jpg',
-                    title: 'Keizersgracht',
-                    text: 'Arte = Amsterdam - Olanda'
-                }
-            ],
-            className: 'slide-carousel',
-            classNameActive: 'active',
-            imageSlide: array.image,
-            
+            array: arrayImageslide, //Più leggibile
+            // className: 'slide-carousel',
+            // classNameActive: 'active',
+            // imageSlide: array.image,
+            currentIndex: 0,
 	    }
     },
     methods: {
+        nextSlide() {
+            console.log('next');
+        },
+        prevSlide() {
+            console.log('prev');
+        }
 	    
     },
 };
